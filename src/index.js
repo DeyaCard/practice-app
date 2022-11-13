5dd6299421c2405c5531772f41fe739a780b600f
Deya Card <thedeyacard@gmail.com>
2022-11-13 13:51:32 -0800

    add inline styling with JSX

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
   return(
-    <div>Hello there!</div>
+    <div>
+      <label for="name" class="label" >Enter e-mail:</label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor:'blue', color:'white'}}>Submit</button>
+    </div>
   )
 }
 
